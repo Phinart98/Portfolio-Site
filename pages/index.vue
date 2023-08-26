05b267544f9051a344475086a63d342cf3c7470d
Philip Narteh <Phinart98@gmail.com>
2023-08-26 18:38:01 +0000

    Created starter code

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button class="rounded bg-blue-500 hover:bg-blue-700 py-2 px-4 text-white">
+      Testing...
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
