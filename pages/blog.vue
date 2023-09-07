570e92ee01326291b53368d5529312b971ca77ad
Philip Narteh <Phinart98@gmail.com>
2023-09-07 00:11:47 +0000

    dark mode stuff

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="flex justify-center">
     <div class="w-full sm:w-3/5">
-      <h2 class="flex justify-center text-3xl mt-8">My Writings</h2>
-      <p class="flex justify-center text-sm mt-2 mb-4">
+      <h2 class="flex justify-center text-3xl mt-8 dark:text-white">My Writings</h2>
+      <p class="flex justify-center text-sm mt-2 mb-4 dark:text-white">
         Welcome to the world of my thoughts
       </p>
       <div
         v-for="(post, index) in blogPosts"
         :key="index"
-        class="h-40 sm:h-32 bg-white mb-4 mx-4 border rounded-lg drop-shadow-lg"
+        class="h-40 sm:h-32 bg-white dark:bg-slate-600 mb-4 mx-4 border rounded-lg drop-shadow-lg"
       >
         <NuxtLink to="">
-          <h5 class="mt-4 mx-8 text-sm">{{ post.date }}</h5>
-          <h1 class="mt-2 mx-8 text-xl font-bold">{{ post.title }}</h1>
+          <h5 class="mt-4 mx-8 text-sm dark:text-white">{{ post.date }}</h5>
+          <h1 class="mt-2 mx-8 text-xl font-bold dark:text-white">{{ post.title }}</h1>
         </NuxtLink>
       </div>
     </div>
