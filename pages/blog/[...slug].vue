296d32c0a8743370177dce2f6d27b3f851dac972
Philip Narteh <Phinart98@gmail.com>
2023-09-09 14:27:02 +0000

    added finishing touches to the blog

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -4,7 +4,7 @@
       class="h-full bg-white dark:bg-slate-800 w-full sm:w-3/5 mx-4 border rounded-sm drop-shadow-lg dark:shadow-black dark:border-black"
     >
       <div
-        class="mx-8 sm:mx-12 my-8 sm:my-12 prose-sm prose-h1:text-center prose-h1:font-medium prose-h2:font-medium"
+        class="mx-8 sm:mx-12 my-8 sm:my-12 prose-sm prose-h1:text-center prose-h1:mb-0 prose-h1:font-medium prose-h2:font-medium prose-a:underline prose-a:underline-offset-4 prose-h1:text-4xl prose-h2:text-2xl dark:text-white"
       >
         <ContentDoc />
       </div>
@@ -18,4 +18,12 @@ definePageMeta({
 });
 </script>
 
-<style></style>
+<style>
+p{
+  @apply text-lg
+}
+
+h6 p {
+  @apply text-center text-sm;
+}
+</style>
